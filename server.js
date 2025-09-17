bca325895b15bc43f1d4def3f3645182c1fd3fff
AlitraDayonic <142802651+AlitraDayonic@users.noreply.github.com>
2025-09-17 01:54:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ const PORT = process.env.PORT || 3000;
 
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public')); // serve frontend
@@ -42,6 +41,7 @@ app.use(cors({
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS']
 }));
+
 
 // const limiter = rateLimit({
  // windowMs: 60 * 1000, // 1 minute
@@ -886,6 +886,7 @@ app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on port ${PORT}`);
    console.log(`📊 Database: ${mysql_url.pathname.slice(1)}`);
 });
+
 
 
 
